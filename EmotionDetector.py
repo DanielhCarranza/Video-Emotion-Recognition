59e41ea076fdee11fa22bb6be8c40b8568a15f69
Danielh Francisco Mendez Carranza <ingbiodanielh@gmail.com>
2018-12-01 19:05:59 -0700

    Emotion Detection

--- EmotionDetector.py
+++ EmotionDetector.py
@@ -5,7 +5,7 @@ import numpy as np
 #import pandas as pd
 #from EmoPy.src.fermodel import FERModel
 import threading
-from EmoPy1.EmoPy.src.fermodel import FERModel
+from Emotion.EmoPy.src.fermodel import FERModel
 
 def Capturing_video():
   counter = 0
