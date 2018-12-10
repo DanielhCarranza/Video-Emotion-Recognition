446049b5e20d7a59c3dcbf4443ec0408f9fcac27
DanielhCarranza <ingbiodanielh@gmail.com>
2018-12-10 08:37:19 -0700

    dsddsc

--- EmotionDetector.py
+++ EmotionDetector.py
@@ -1,14 +1,13 @@
 import cv2 
 from time import sleep 
-from PIL import Image
 import numpy as np
 import pandas as pd
 import threading
 from Emotion.EmoPy.src.fermodel import FERModel
 
-def Capturing_video():
+def Capturing_video(video):
   counter = 0
-  CapturedVideo = cv2.VideoCapture('ObamaVideo.mp4')
+  CapturedVideo = cv2.VideoCapture(video)
   FrameRate = CapturedVideo.get(5)
   success = True
   while success:
@@ -19,13 +18,19 @@ def Capturing_video():
         counter += 1
 
 
-def Predicting(im, index):
+def Predicting(model,im, index):
   cv2.imwrite("Photo.jpg", im)
   print('Predicting emotion of Frame %d...' % (index+1))
   index += 1
-  p, acc = model.predict("Photo.jpg")
-  print(f'Dominant: {p}, acc: {acc*100}')
-  return p, acc
+  list_Emotions=[]
+  list_accuracy=[]
+  
+  for i in range(len(model)):
+      emotion, accuracy = model['Model'+str(i)].predict("Photo.jpg")
+      list_Emotions.append(emotion)
+      list_accuracy.append(accuracy)
+      print(f'Dominant: {emotion}, acc: {accuracy*100}, Model {i}')
+  return list_Emotions, list_accuracy
 
 
 def Model():
@@ -38,7 +43,7 @@ def Model():
             fear, anger, disgust
             surprise, calm, disgust
             surprise, sadness, disgust
-            happiness, ange """
+            happiness, anger """
 
     Emotions = [['calm', 'anger', 'happiness'],
                 ['surprise', 'disgust', 'happiness'],
@@ -63,43 +68,40 @@ def Model():
     return models
 
 if __name__ =='__main__':
-
-
-    target_emotions = ['calm', 'anger', 'happiness']
-    target_emotions2 = ['surprise', 'disgust', 'happiness']
-
-    model = FERModel(target_emotions, verbose=True)
-    model2 = FERModel(target_emotions2, verbose=True)
+    # Lista de Modelos 
+    model=Model()
 
     im = []    
     emotions=[]
-    percentage=[]
-    Capturing_video = threading.Thread(target=Capturing_video)
+    accuracy=[]
+    
+    Capturing_video = threading.Thread(target=Capturing_video('ObamaVideo.mp4'))
     Capturing_video.start()
     sleep(2)
     num = 0
+
     try:
         while True:
-            em,acc=Predicting(im[num], num)
+            em,acc=Predicting(model, im[num], num)
             emotions.append(em)
-            percentage.append(acc)
+            accuracy.append(acc)
             num += 1
             sleep(1)
     except:
+        print('==========ANALISIS==TERMINADO=================')
         print('==============================================')
         print('==============================================')
         print('==============================================')
-        print('==============================================')
-        EmotionsM= pd.DataFrame(np.vstack((emotions,percentage)).T,
-                     columns=['Emotions','Accuracy'])
-        EmotionsM['Accuracy']=EmotionsM['Accuracy'].astype('float')
-        EmotionsM['Emotions']=EmotionsM['Emotions'].astype('category')
+        
+        emotions= pd.DataFrame(np.array(emotions).reshape(31*9,-1),columns=['Emotions'])
+        accuracy= pd.DataFrame(np.array(accuracy).reshape(31*9,-1),columns=['Accuracy'])
+        EmotionsM=pd.concat([emotions, accuracy], axis=1)
         Emotions=EmotionsM.set_index('Emotions')
         
         print('==============================================')
         emo, cantidad=np.unique(EmotionsM.iloc[:,0], return_counts=True)
-        print(f'Emotions: {emo}, Cantidad Detectada : {cantidad}')
-
+        print(f'Emotions: {emo}\n Cantidad Detectada : {cantidad}')
+        
         for i in emo:
             mean_Emotion = Emotions.loc[i].mean()
             print(f'{i} AVG {mean_Emotion} ' )
